4ade19ea8c4f9cace0cf34e18347ef06e779aaff
kyqa <126547641+kyqa@users.noreply.github.com>
2023-03-28 20:33:53 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,2 +1,3 @@
 loadstring(game:HttpGet('https://raw.githubusercontent.com/kyqa/Jersey-Script/main/script.lua'))()
+wait(3)
 loadstring(game:HttpGet('https://raw.githubusercontent.com/kyqa/Stadium-Color/main/script.lua'))()
